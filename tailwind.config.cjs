36a880dc844dbab918b4e472b0ae4fd43ce00fa2
Lisbeth Collin <lico0001@stud.kea.dk>
2024-05-28 14:38:41 +0200

    inner shadow

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -76,7 +76,7 @@ module.exports = {
         drop: "-2px 2px 10px rgba(0, 0, 0, 0.25)",
       },
       boxShadow: {
-        inner: "inset 1px -2px 3px 0 rgba(0, 0, 0, 0.25)",
+        inner: "inset 1px -2px 3px 0 rgba(0, 0, 0, 0.10)",
       },
     },
   },
